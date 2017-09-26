8f6f2cb8a46c07098d4e4c2a97afaec6a0f5d22a
XiaoWuMac <572518791@qq.com>
2017-09-26 13:16:10 +0800

    test

--- app/src/main/java/com/xiaowu/myself/app/base/MvpBaseActivity.java
+++ app/src/main/java/com/xiaowu/myself/app/base/MvpBaseActivity.java
@@ -1,9 +1,11 @@
 package com.xiaowu.myself.app.base;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by xiaowumac on 2017/9/8.
  */
 
-public class MvpBaseActivity {
+public class MvpBaseActivity extends AppCompatActivity{
 
 }
